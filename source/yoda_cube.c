7138302e493a4dad83e97e74267cbecc5e4913cb
sandyx86 <sandyx.code@proton.me>
2023-11-23 05:31:50 -0600

    apply textures

--- source/yoda_cube.c
+++ source/yoda_cube.c
@@ -11,11 +11,13 @@
 
 
 #define DEFAULT_FIFO_SIZE (256*1024)
+#define VTX_BUFFER_SIZE 1024*sizeof(f32);
 GXRModeObj *rmode;
 static void *xfb[2] = {NULL, NULL}; //external framebuffer
+static float *vb;
+
 static u32 curr_fb = 0;
 //static unsigned int do_copy = false;
-
 GXTexObj texObj; //texture object
 TPLFile tpl; //tpl file
 
@@ -31,6 +33,7 @@ s16 cube[] ATTRIBUTE_ALIGN(32) = {
 	-1, -1,  1,  // 6
 	-1, -1, -1,  // 7
 };
+
 
 s16 scuffed_cube[] ATTRIBUTE_ALIGN(32) = {
 	// x y z
@@ -138,8 +141,6 @@ int main(void) {
     VIDEO_Init();
     PAD_Init();
 
-    
-
     rmode = VIDEO_GetPreferredMode(NULL);
     xfb[0] = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
     xfb[1] = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
@@ -188,30 +189,41 @@ int main(void) {
     GX_ClearVtxDesc();
     GX_SetVtxDesc(GX_VA_POS, GX_INDEX8);
 	GX_SetVtxDesc(GX_VA_CLR0, GX_INDEX8);
+    GX_SetVtxDesc(GX_VA_TEX0, GX_DIRECT);
 
 
     scaleArray(cube, 8*3, 30);
+    /*
     translateArray(cube, 8*3, (guVector){10, 0, 0}); //move cube
-    s16 *array = aligned_alloc(16 * sizeof(s16)*3, 64);
-    memcpy(array, cube, 8 * sizeof(s16)*3); //move cube into vertex array
+    f32 *array = aligned_alloc(16 * sizeof(f32)*3, 64);
+    memcpy(array, cube, 8 * sizeof(f32)*3); //move cube into vertex array
     //scaleArray(array, 8*3, 30); //make the shapes larger
     translateArray(cube, 8*3, (guVector){-150, 0, 0}); //move cube back
-    memcpy(array + 3*8, cube, 8 * sizeof(s16)*3);
+    memcpy(array + 3*8, cube, 8 * sizeof(f32)*3);
+    */
 
+    TPL_OpenTPLFromMemory(&tpl, (void *)textures_tpl, textures_tpl_size);
+    TPL_GetTexture(&tpl, yoda, &texObj);
+    GX_LoadTexObj(&texObj, GX_TEXMAP0);
+
+    GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_TEX0, GX_TEX_ST, GX_F32, 0);
     GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_POS, GX_POS_XYZ, GX_S16, 0);
 	GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_CLR0, GX_CLR_RGBA, GX_RGBA8, 0);
-    GX_SetArray(GX_VA_POS, array, 3*sizeof(s16)); //put the cube in pos memory
+    GX_SetArray(GX_VA_POS, cube, 3*sizeof(s16)); //put the cube in pos memory
 	GX_SetArray(GX_VA_CLR0, colors, 4*sizeof(u8)); //put the colors in color memory
+    GX_SetArray(GX_VA_TEX0, (void *)textures_tpl, textures_tpl_size);
     
     //GX_SetArray(GX_VA_POS, cube, 3*sizeof(s16));
 
     DCFlushRange(cube, 65536);
 	DCFlushRange(colors, 65536);
+    DCFlushRange((void *)textures_tpl, 65536);
     
     GX_SetNumChans(1); //set number of color channels that are output to TEV stages
-    GX_SetNumTexGens(0); //set number of texture coordinates that are available in TEV
-    GX_SetTevOrder(GX_TEVSTAGE0, GX_TEXCOORDNULL, GX_TEXMAP_NULL, GX_COLOR0A0);
-	GX_SetTevOp(GX_TEVSTAGE0, GX_PASSCLR);
+    GX_SetNumTexGens(1); //set number of texture coordinates that are available in TEV
+    //GX_SetTexCoordGen(GX_TEXCOORD0, GX_TG_MTX2x4, GX_TG_TEX0, GX_IDENTITY);
+    GX_SetTevOrder(GX_TEVSTAGE0, GX_TEXCOORD0, GX_TEXMAP0, GX_COLOR0A0);
+	GX_SetTevOp(GX_TEVSTAGE0, GX_DECAL);;
 
     Mtx44 view; //view matrix
     Camera cam = {
@@ -227,11 +239,11 @@ int main(void) {
     //game loop
     while(true) {
         guLookAt(view, &cam.pos, &cam.up, &cam.view);
-
-        //guLookAt(view, &cam, &up, &look);
+;
         GX_SetViewport(0,0,rmode->fbWidth,rmode->efbHeight,0,1);
         GX_InvVtxCache();
         GX_InvalidateTexAll();
+
         PAD_ScanPads();
         int stickX = PAD_StickX(0);
         int stickY = PAD_StickY(0);
@@ -240,15 +252,14 @@ int main(void) {
         //press A to switch models
         if (PAD_ButtonsDown(0) & PAD_BUTTON_RIGHT) {
             //scaleArray(array, 8*3, 2);
-            translateArray(array, 8*3, (guVector){1, 0, 0});
+            //translateArray(array, 8*3, (guVector){1, 0, 0});
         }
 
         if (PAD_ButtonsDown(0) & PAD_BUTTON_LEFT) {
             //scaleArray(array, 8*3, 0.5F);
-            translateArray(array, 8*3, (guVector){-1, 0, 0});
+            //translateArray(array, 8*3, (guVector){-1, 0, 0});
         }
 
-        GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_POS, GX_POS_XYZ, GX_S16, 0);
         //GX_SetArray(GX_VA_POS, big_array, 3*sizeof(s16));
 
         moveCamera(&cam, stickX, stickY, rotation);
@@ -263,6 +274,19 @@ int main(void) {
         VIDEO_Flush();
         VIDEO_WaitVSync();
     }
+}
+
+void drawVert(u8 pos, u8 c, f32 s, f32 t) {
+    GX_Position1x8(pos);
+	GX_Color1x8(c);
+	GX_TexCoord2f32(s, t);
+}
+
+void drawQuad(u8 v0, u8 v1, u8 v2, u8 v3, u8 c) {
+    drawVert(v0, 0, 0.0, 0.0);
+    drawVert(v1, 0, 1.0, 0.0);
+    drawVert(v2, 0, 1.0, 1.0);
+    drawVert(v3, 0, 0.0, 1.0);
 }
 
 void draw_quad(u8 v0, u8 v1, u8 v2, u8 v3, u8 c) {
@@ -299,22 +323,32 @@ void drawModel(Mtx v, Camera *cam) {
     GX_LoadPosMtxImm(modelview, GX_PNMTX0);
 
     //will draw whatever is in POS_VA
-    GX_Begin(GX_QUADS, GX_VTXFMT0, 48);
+    GX_Begin(GX_QUADS, GX_VTXFMT0, 24);
     //maybe make an array of draw functions that get called here
+        /*
         draw_quad(0, 3, 2, 1, 0); //top face
 		draw_quad(0, 7, 6, 3, 1); //left face
 		draw_quad(0, 1, 4, 7, 2); //back face
 		draw_quad(1, 2, 5, 4, 3); //right face
 		draw_quad(2, 3, 6, 5, 4); //front face
 		draw_quad(4, 7, 6, 5, 5); //bottom face
-
+        */
         
-        draw_quad(0+8, 3+8, 2+8, 1+8, 0); //top face
-		draw_quad(0+8, 7+8, 6+8, 3+8, 1); //left face
-		draw_quad(0+8, 1+8, 4+8, 7+8, 2); //back face
-		draw_quad(1+8, 2+8, 5+8, 4+8, 3); //right face
-		draw_quad(2+8, 3+8, 6+8, 5+8, 4); //front face
-		draw_quad(4+8, 7+8, 6+8, 5+8, 5); //bottom face
+        drawQuad(0, 3, 2, 1, 0); //top face
+		drawQuad(0, 7, 6, 3, 1); //left face
+		drawQuad(0, 1, 4, 7, 2); //back face
+		drawQuad(1, 2, 5, 4, 3); //right face
+		drawQuad(2, 3, 6, 5, 4); //front face
+		drawQuad(4, 7, 6, 5, 5); //bottom face
+        
+        /*
+        drawQuad(0+8, 3+8, 2+8, 1+8, 0); //top face
+		drawQuad(0+8, 7+8, 6+8, 3+8, 1); //left face
+		drawQuad(0+8, 1+8, 4+8, 7+8, 2); //back face
+		drawQuad(1+8, 2+8, 5+8, 4+8, 3); //right face
+		drawQuad(2+8, 3+8, 6+8, 5+8, 4); //front face
+		drawQuad(4+8, 7+8, 6+8, 5+8, 5); //bottom face
+        */
 
     GX_End();
 }
