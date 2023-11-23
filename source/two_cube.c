e91d11b014e50079f358d6595089ebe123283dc3
sandyx86 <sandyx.code@proton.me>
2023-11-23 04:09:46 -0600

    draw a second cube

--- source/two_cube.c
+++ source/two_cube.c
@@ -19,17 +19,46 @@ static u32 curr_fb = 0;
 GXTexObj texObj; //texture object
 TPLFile tpl; //tpl file
 
+
 s16 cube[] ATTRIBUTE_ALIGN(32) = {
 	// x y z
-	-30,  30, -30, 	// 0
-	 30,  30, -30, 	// 1
-	 30,  30,  30, 	// 2
-	-30,  30,  30, 	// 3
-	 30, -30, -30, 	// 4
-	 30, -30,  30, 	// 5
-	-30, -30,  30,  // 6
-	-30, -30, -30,  // 7
+	-1,  1, -1, 	// 0
+	 1,  1, -1, 	// 1
+	 1,  1,  1, 	// 2
+	-1,  1,  1, 	// 3
+	 1, -1, -1, 	// 4
+	 1, -1,  1, 	// 5
+	-1, -1,  1,  // 6
+	-1, -1, -1,  // 7
 };
+
+s16 scuffed_cube[] ATTRIBUTE_ALIGN(32) = {
+	// x y z
+	-60,  60, -60, 	// 0
+	 60,  20, -60, 	// 1
+	 60,  60,  10, 	// 2
+	-60,  60,  60, 	// 3
+	 60, -30, -60, 	// 4
+	 60, -60,  60, 	// 5
+	-60, -60,  60,  // 6
+	-60, -60, -60,  // 7
+};
+
+u8 pyramid[] ATTRIBUTE_ALIGN(32) = {
+    -60,  60, -60, 	// 0
+	 60,  20, -60, 	// 1
+	 60,  60,  10, 	// 2
+	-60,  60,  60, 	// 3
+	 60, -30, -60, 	// 4
+};
+
+s16 triangle[] ATTRIBUTE_ALIGN(32) = {
+    -30, -30, 0,
+    30, -30, 0,
+    0, 30, 0,
+};
+
+s16 *cubeSwitch[2] = {cube, scuffed_cube};
 
 u8 colors[] ATTRIBUTE_ALIGN(32) = {
 	// r, g, b, a
@@ -41,14 +70,65 @@ u8 colors[] ATTRIBUTE_ALIGN(32) = {
 	  0,  20, 100, 255  // 5 blue
 };
 
+//use this struct
+typedef struct _Shape {
+    guVector pos; //location in worldspace
+    guVector scale; //scale of object
+    guVector axis; //rotation axis
+    float rot; //rotation in worldspace
+    int vertc; //number of verts
+    s16* verts; //pointer to array of verts
+}Shape;
+
+void translateShape(Shape *shape) {
+    for (int i = 0; i < shape->vertc; i+=3) {
+        shape->verts[i+0] = shape->verts[i+0] + shape->pos.x;
+        shape->verts[i+1] = shape->verts[i+1] + shape->pos.y;
+        shape->verts[i+2] = shape->verts[i+2] + shape->pos.z;
+    }
+}
+
+void scaleShape(Shape *shape) {
+    for (int i = 0; i < shape->vertc; i+=3) {
+        shape->verts[i+0] = shape->verts[i+0] * shape->scale.x;
+        shape->verts[i+1] = shape->verts[i+1] * shape->scale.y;
+        shape->verts[i+2] = shape->verts[i+2] * shape->scale.z;
+    }
+}
+
+void scaleArray(s16 *va, int c, int scale) {
+    for (int i = 0; i < c; i++) {
+        va[i] = va[i] * scale;
+    }
+}
+
+void translateArray(s16 *va, int c, guVector vec) {
+    for (int i = 0; i < c; i+=3) {
+        va[i] += vec.x;
+        va[i+1] += vec.y;
+        va[i+2] += vec.z;
+    }
+}
+
+//make a vertex array from two shapes
+s16 *vtxcat(Shape *s1, Shape *s2) {
+    size_t size = (s1->vertc + s2->vertc) * sizeof(guVector);
+    s16 *va = aligned_alloc(size, 32);
+    memset(va, 0, size);
+    memcpy(va, s1->verts, s1->vertc * sizeof(guVector));
+    
+    //memcpy(va + s1->vertc, s2->verts, s2->vertc * sizeof(s16)*3);
+    return va;
+}
+
 typedef struct _Camera {
     guVector pos;
     guVector up;
     guVector view;
 } Camera;
 
-void draw_cube(Mtx v, Camera *cam);
-
+void drawModel(Mtx v, Camera *cam);
+void drawFace(u8,u8,u8,u8);
 void moveCamera(Camera *cam, int stickX, int stickY, float rotation);
 void rotateCamera(Mtx, int);
 
@@ -57,6 +137,8 @@ int main(void) {
     void *gp_fifo = NULL; //CPU to GP FIFO buffer for the GP's command processor
     VIDEO_Init();
     PAD_Init();
+
+    
 
     rmode = VIDEO_GetPreferredMode(NULL);
     xfb[0] = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
@@ -107,14 +189,24 @@ int main(void) {
     GX_SetVtxDesc(GX_VA_POS, GX_INDEX8);
 	GX_SetVtxDesc(GX_VA_CLR0, GX_INDEX8);
 
+
+    scaleArray(cube, 8*3, 30);
+    translateArray(cube, 8*3, (guVector){10, 0, 0}); //move cube
+    s16 *array = aligned_alloc(16 * sizeof(s16)*3, 64);
+    memcpy(array, cube, 8 * sizeof(s16)*3); //move cube into vertex array
+    //scaleArray(array, 8*3, 30); //make the shapes larger
+    translateArray(cube, 8*3, (guVector){-150, 0, 0}); //move cube back
+    memcpy(array + 3*8, cube, 8 * sizeof(s16)*3);
+
     GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_POS, GX_POS_XYZ, GX_S16, 0);
 	GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_CLR0, GX_CLR_RGBA, GX_RGBA8, 0);
-    
-    GX_SetArray(GX_VA_POS, cube, 3*sizeof(s16)); //put the cube in pos memory
+    GX_SetArray(GX_VA_POS, array, 3*sizeof(s16)); //put the cube in pos memory
 	GX_SetArray(GX_VA_CLR0, colors, 4*sizeof(u8)); //put the colors in color memory
+    
+    //GX_SetArray(GX_VA_POS, cube, 3*sizeof(s16));
 
-    DCFlushRange(cube,sizeof(cube));
-	DCFlushRange(colors,sizeof(colors));
+    DCFlushRange(cube, 65536);
+	DCFlushRange(colors, 65536);
     
     GX_SetNumChans(1); //set number of color channels that are output to TEV stages
     GX_SetNumTexGens(0); //set number of texture coordinates that are available in TEV
@@ -128,7 +220,9 @@ int main(void) {
         {0.0, 0.0, -1.0} //looking down -z axis
     };
 
-    static float rotation = 0.0; //theta
+    static float rotation = 0.0;
+    static int scale = 1;
+    int swap = 0;
 
     //game loop
     while(true) {
@@ -143,8 +237,22 @@ int main(void) {
         int stickY = PAD_StickY(0);
         rotation += PAD_SubStickX(0)/20.0;
 
+        //press A to switch models
+        if (PAD_ButtonsDown(0) & PAD_BUTTON_RIGHT) {
+            //scaleArray(array, 8*3, 2);
+            translateArray(array, 8*3, (guVector){1, 0, 0});
+        }
+
+        if (PAD_ButtonsDown(0) & PAD_BUTTON_LEFT) {
+            //scaleArray(array, 8*3, 0.5F);
+            translateArray(array, 8*3, (guVector){-1, 0, 0});
+        }
+
+        GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_POS, GX_POS_XYZ, GX_S16, 0);
+        //GX_SetArray(GX_VA_POS, big_array, 3*sizeof(s16));
+
         moveCamera(&cam, stickX, stickY, rotation);
-        draw_cube(view, &cam); //draws a cube in the view matrix
+        drawModel(view, &cam); //draws the world
 
         GX_SetZMode(GX_TRUE, GX_LEQUAL, GX_TRUE);
         GX_SetColorUpdate(GX_TRUE);
@@ -174,8 +282,8 @@ void moveCamera(Camera *cam, int stickX, int stickY, float rotation) {
     cam->pos.z += -PAD_StickX(0)*sinf(DegToRad(rotation))/20.0 + PAD_StickY(0)*cosf(DegToRad(rotation))/20.0;
 }
 
-void draw_cube(Mtx v, Camera *cam) {
-    Mtx model, modelview, xorz;
+void drawModel(Mtx v, Camera *cam) {
+    Mtx model, modelview;
     static guVector axis = {0, 1, 0};
     static float rotation = 0.0;
     
@@ -185,16 +293,38 @@ void draw_cube(Mtx v, Camera *cam) {
     guMtxRotAxisDeg(v, &axis, rotation); //rotate view around y axis
     guMtxTransApply(model, model, cam->pos.x, cam->pos.y, cam->pos.z); //translate world relative to camera
     guMtxConcat(v, model, modelview);
+    //guMtxConcat(model, modelview, modelview);
 
     //load the modelview matrix into matrix memory
     GX_LoadPosMtxImm(modelview, GX_PNMTX0);
-    GX_Begin(GX_QUADS, GX_VTXFMT0, 24);
+
+    //will draw whatever is in POS_VA
+    GX_Begin(GX_QUADS, GX_VTXFMT0, 48);
     //maybe make an array of draw functions that get called here
-        draw_quad(0, 3, 2, 1, 0);
-		draw_quad(0, 7, 6, 3, 1);
-		draw_quad(0, 1, 4, 7, 2);
-		draw_quad(1, 2, 5, 4, 3);
-		draw_quad(2, 3, 6, 5, 4);
-		draw_quad(4, 7, 6, 5, 5);
+        draw_quad(0, 3, 2, 1, 0); //top face
+		draw_quad(0, 7, 6, 3, 1); //left face
+		draw_quad(0, 1, 4, 7, 2); //back face
+		draw_quad(1, 2, 5, 4, 3); //right face
+		draw_quad(2, 3, 6, 5, 4); //front face
+		draw_quad(4, 7, 6, 5, 5); //bottom face
+
+        
+        draw_quad(0+8, 3+8, 2+8, 1+8, 0); //top face
+		draw_quad(0+8, 7+8, 6+8, 3+8, 1); //left face
+		draw_quad(0+8, 1+8, 4+8, 7+8, 2); //back face
+		draw_quad(1+8, 2+8, 5+8, 4+8, 3); //right face
+		draw_quad(2+8, 3+8, 6+8, 5+8, 4); //front face
+		draw_quad(4+8, 7+8, 6+8, 5+8, 5); //bottom face
+
     GX_End();
+}
+
+//draw a single triangle
+void drawFace(u8 x, u8 y, u8 z, u8 c) {
+    GX_Position1x8(x);
+    GX_Color1x8(c);
+    GX_Position1x8(y);
+    GX_Color1x8(c);
+    GX_Position1x8(z);
+    GX_Color1x8(c);
 }
